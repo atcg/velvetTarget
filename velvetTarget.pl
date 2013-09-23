b83d3ee300292c44fdfdb07d193b361a5e450071
atcg <evanmelstad@ucla.edu>
2013-09-23 02:01:48 -0700

    Now blasts against supplied probeset and calculates summary stats separately for each blast report.

--- velvetTarget.pl
+++ velvetTarget.pl
@@ -25,6 +25,8 @@ use strict;
 use warnings;
 use Getopt::Long;
 use Pod::Usage;
+use File::Basename qw(fileparse);
+use Bio::SearchIO;
 
 my $help = 0;
 my $miSeq = 0;
@@ -35,23 +37,25 @@ my $singlesFile;
 my $fromK = 19; #default starting Kmer value is 19
 my $toK = 201; #default ending emer value is 201
 my $phredBase = 33; #defaults to phred33 quality scores
+my $probes;
 my $outFile;
 my $usage = "Usage: perl velvetTarget.pl -i <infile.txt> -o <outfile.txt>\n";
 
-GetOptions  ("name=s"    => \$name,
-             "R1=s"      => \$R1file,
-             "R2=s"      => \$R2file,
-             "singles=s" => \$singlesFile,
-             "from=i"    => \$fromK,
-             "to=i"      => \$toK,
-             "phred=i"   => \$phredBase,
-             "out=s"     => \$outFile,
-             "miseq"     => \$miSeq,
-             "help|man"  => \$help,) || pod2usage(2);
+GetOptions  ("name=s"           => \$name,
+             "R1=s"             => \$R1file,
+             "R2=s"             => \$R2file,
+             "singles=s"        => \$singlesFile,
+             "from=i"           => \$fromK,
+             "to=i"             => \$toK,
+             "phred=i"          => \$phredBase,
+             "out=s"            => \$outFile,
+             "probes|targets=s" => \$probes,
+             "miseq"            => \$miSeq,
+             "help|man"         => \$help,) || pod2usage(2);
 
 pod2usage(-exitval => 0, -verbose => 2, -noperldoc => 1) if $help;
 
-if ((!$name) or (!$R1file) or (!$R2file) or (!$singlesFile) or (!$fromK) or (!$toK) or (!$outFile)) {
+if ((!$name) or (!$R1file) or (!$R2file) or (!$singlesFile) or (!$fromK) or (!$toK) or (!$outFile) or (!$probes)) {
     pod2usage(-exitval => 0, -verbose => 2, -noperldoc => 1);
 }
 if (($fromK % 2 != 1) or ($toK % 2 != 1)) {
@@ -60,14 +64,17 @@ if (($fromK % 2 != 1) or ($toK % 2 != 1)) {
 }
 
 
-
-
 # Run velveth for all odd numbers between the specified kmer values
 my @kmer_choices = grep {$_ % 2 == 1} $fromK..$toK;
+my %blastingStats; # Holds information regarding the number of targets matching contigs and completeness of match
 
 foreach my $kmer (@kmer_choices) {
     my $velvet_dir_name = $name . "_" . $kmer . "_velvet";
     my $dbName = $name . "_" . $kmer;
+    my ($probeFilename) = basename($probes);
+    my $blastOutName = $probeFilename . "_blasted_to_" . $dbName;
+    my $blastStatsLog = $velvet_dir_name . "/$blastOutName" . "_blastStats.txt";
+    
     if ($miSeq) {
         system("velveth $velvet_dir_name $kmer -short -fastq $singlesFile -longPaired -separate -fastq $R1file $R2file");
         system("velvetg $velvet_dir_name -exp_cov auto -cov_cutoff auto");
@@ -75,9 +82,52 @@ foreach my $kmer (@kmer_choices) {
         system("velveth $velvet_dir_name $kmer -short -fastq $singlesFile -shortPaired -separate -fastq $R1file $R2file");
         system("velvetg $velvet_dir_name -exp_cov auto -cov_cutoff auto");
     }
-    system("makeblastdb -in $velvet_dir_name/contigs.fa -dbtype nucl -out $velvet_dir_name/$dbName");        
-}
+    system("makeblastdb -in $velvet_dir_name/contigs.fa -dbtype nucl -out $velvet_dir_name/$dbName");
+    
+    # Blast targets against assemblies
+    system("blastn -db $velvet_dir_name/$dbName -query $probes -out $velvet_dir_name/$blastOutName");
+    
+    #Compile summary stats of blast results. We want to know:
+    # 1. How many targets have at least one matching contig when blasted against the assembly?
+    $blastingStats{'targets_with_hits'} = 0;
+    # 2. How many targets have exactly one matching contig (hit) when blasted against the assembly?
+    $blastingStats{'targets_with_one_hit'} = 0;
+    # 3. How many targets with a single hit have exactly one HSP when blasted against the assembly?
+    $blastingStats{'targets_with_one_hsp'} = 0;
+    # 4. How many targets with a single hsp are matched to at least 98% of their length (i.e. are nested within a contig with overlap)?
+    # Note this measure might need tweaking. For instance, if the first basepair of the query sequence is mismatched, but all remaining
+    # basepairs are a match, does blast chop off the first basepair and report the alignment as, say 119bp out of a 120bp query. This could be
+    # misleading if the first or last base pairs are simply mutations from the query.
+    $blastingStats{'targets_nested_within_contig'} = 0;
 
+    my $blastIO = Bio::SearchIO->new(-format => 'blast', -file => "$velvet_dir_name/$blastOutName");
+    while (my $blastResult = $blastIO->next_result()) {
+        if ($blastResult->num_hits() > 0) {
+            $blastingStats{'targets_with_hits'}++;
+            if ($blastResult->num_hits() == 1) {
+                $blastingStats{'targets_with_one_hit'}++;
+                while (my $hit = $blastResult->next_hit()) {
+                    if ($hit->num_hsps() == 1) {
+                        $blastingStats{'targets_with_one_hsp'}++;
+                        my $hsp = $hit->next_hsp();
+                        if ((($hsp->length('total') / $blastResult->query_length()) > 0.98) and ($hsp->start('subject') != 1) and ($hsp->end('subject') != 1)) { #try changing 'total' to 'hit' in the length call to see what happens
+                            $blastingStats{'targets_nested_within_contig'}++
+                        }
+                    }            
+                }       
+            }   
+        }
+    }
+    open(my $STATSFH, ">", $blastStatsLog) or die "Couldn't create the file to store the blast stats.\n";
+    print $STATSFH "Number of target regions: " . $blastIO->result_count() . ".\n";
+    print $STATSFH "Number of target regions with hits: $blastingStats{'targets_with_hits'}. (" . ((($blastingStats{'targets_with_hits'})/($blastIO->result_count))*100) . "% of total)\n";
+    print $STATSFH "Number of target regions with ONE hit: $blastingStats{'targets_with_one_hit'} (" . ((($blastingStats{'targets_with_one_hit'})/($blastIO->result_count))*100) . "% of total)\n";
+    print $STATSFH "Number of target regions with ONE HSP: $blastingStats{'targets_with_one_hsp'} (" . ((($blastingStats{'targets_with_one_hsp'})/($blastIO->result_count))*100) . "% of total)\n";
+    print $STATSFH "Number of targets with at least 98% of their length within a contig: $blastingStats{'targets_nested_within_contig'} (" . ((($blastingStats{'targets_nested_within_contig'})/($blastIO->result_count))*100) . "% of total)\n";
+    
+    close($STATSFH) or die "Can't close blast stats summary file handle.\n";
+    
+}
 
 
 
@@ -111,12 +161,13 @@ foreach my $kmer (@kmer_choices) {
 __END__
 
 =head1 NAME
+
 velvetTarget.pl
 
 =head1 SYNOPSIS 
 
 perl velvetTarget.pl --R1 <file> --R2 <file> --singles <file> --from \
-<int> --to <int> --phred <int> --out <file>
+<int> --to <int> --phred <int> --out <file> --probes <file>
 
  Options:
    -R1=s            R1 reads file
@@ -126,6 +177,7 @@ perl velvetTarget.pl --R1 <file> --R2 <file> --singles <file> --from \
    -to=i            Ending kmer value (default 201)
    -phred=i         Phred quality scoring (default = 33)
    -out=s           Log file name
+   -probes=s        A fasta file containing the probes or target regions
    -miseq           Include this flag if you have MiSeq reads over 200bp
    -help|man        documentation
 
@@ -143,6 +195,6 @@ This script attempts to find the k value that achieves this. Instead of
 basing the choice of k off of summary statistics of the assembled
 contigs themselves, it rather blasts the baits or target regions against
 the assembly, and returns the k value and assembly that recovers the
-most targets the most completely. 
+most targets the most completely.
 
 =cut
